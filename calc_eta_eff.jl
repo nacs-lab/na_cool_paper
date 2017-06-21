df0fd8321c63e6b852493e9180e62e88475f77f1
Yichao Yu <yyc1992@gmail.com>
2017-06-21 21:44:52 +0800

    Address some of Nick's comments
    * Most of the error bars
    * Figure label update
    * More explaination of anharmonicity
    * More detail about improvements to be made

--- calc_eta_eff.jl
+++ calc_eta_eff.jl
@@ -5,23 +5,24 @@ import NaCsCalc: Trap
 
 # Matrix elements
 const m_Na = 23e-3 / 6.02e23
-const ν_a1 = 68.8e3
-const ν_r2 = 430e3
-const ν_r3 = 589.5e3
-const η_a1_op = Trap.η(m_Na, ν_a1, 2π / 589e-9)
-const η_r2_op = Trap.η(m_Na, ν_r2, 2π / 589e-9)
-const η_r3_op = Trap.η(m_Na, ν_r3, 2π / 589e-9)
-const η_a1 = η_a1_op * 0.67
-const η_r2 = η_r2_op * √(2)
-const η_r3 = η_r3_op * √(2)
+const νs = (69e3, 430e3, 590e3)
+const ηs_op = Trap.η.(m_Na, νs, 2π / 589e-9)
+const ηs_ra = ηs_op .* (0.67, √2, √2)
 
-function calc_η_eff(TμK)
+function calc_nbar(TμK)
     ħ = 1.0545718e-34
     k_B = 1.38064852e-23
     Hz_per_μK = k_B * 1e-6 / ħ / 2π
     THz = TμK * Hz_per_μK
-    nbars = THz ./ (ν_a1, ν_r2, ν_r3)
-    return sqrt.(2 .* nbars .+ 1) .* (η_a1_op, η_r2_op, η_r3_op)
+    return THz ./ νs
 end
 
+function calc_η_eff(TμK)
+    return sqrt.(2 .* calc_nbar(TμK) .+ 1) .* ηs_op
+end
+
+@show ηs_op
+@show ηs_ra
+@show calc_nbar(70)
 @show calc_η_eff(70)
+# @show calc_η_eff(75)
